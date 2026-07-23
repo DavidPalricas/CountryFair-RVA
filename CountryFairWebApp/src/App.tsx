23fe6f82635175af4ca07c918bf4cf3be1fc54f4
davidpalricas <davipalricas22@gmail.com>
2026-07-23 14:17:59 +0100

    WebApp Style Updated

--- CountryFairWebApp/src/App.tsx
+++ CountryFairWebApp/src/App.tsx
@@ -7,14 +7,27 @@ function App() {
   return (
    <div className="App">
       <Canvas>
-       /* Game Stuff */
+       {/* Game Stuff */}
       </Canvas>
 
       <div className="info">
-        <h1>Bem vindo ao Country Fair VR</h1>
-        <img src={zecaImg} alt="ZecaBigodes" />
+        <div className="signboard">
+          <h1 className="signboard__title">Bem vindo ao Country Fair VR</h1>
 
-         <h1>Esperando que o se conecte ao jogo</h1>
+          <div className="signboard__body">
+            <img src={zecaImg} alt="ZecaBigodes" />
+
+            <div className="signboard__status">
+              <h1>Esperando que ligue ao jogo</h1>
+
+              <div className="waiting-dots" aria-hidden="true">
+                <span />
+                <span />
+                <span />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
